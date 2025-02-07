f2ff34a402ae0ce534d26d01252944282250da40
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-02-07 06:07:19 +0100

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KafkaModule } from './kafka/kafka.module';
-import { EmployeeModule } from './employee/employee.module';
+import { EmployeeModule } from './modules/employee/employee.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
