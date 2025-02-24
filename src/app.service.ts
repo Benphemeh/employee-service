2104f9db39e8fcfad0ef3c7ae688045d423a4598
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-02-24 08:47:48 +0100

    mongo Network access fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'YAY! welcome to employee service';
+    return 'YAaY!!! welcome to employee service';
   }
 }
